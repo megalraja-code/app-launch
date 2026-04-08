383ff4812177fb6b28fc68170f5a6e4d5d1544a7
Megalraja <megalrajaa@gmail.com>
2026-04-08 16:34:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// Set launch date
+const launchDate = new Date("May 1, 2026 00:00:00").getTime();
+
+const timer = setInterval(() => {
+  const now = new Date().getTime();
+  const distance = launchDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+  document.getElementById("seconds").innerText = seconds;
+
+  if (distance < 0) {
+    clearInterval(timer);
+    document.querySelector(".countdown").innerHTML = "🎉 Launched!";
+  }
+
+}, 2000);
